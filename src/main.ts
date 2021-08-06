b7c6a593ae06a368c46bd8d3b05cd7a20aea3861
Maddie Beyl <maddie@revenuecat.com>
2021-08-06 09:54:31 -0700

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { Purchases } from '@ionic-native/purchases'
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -8,5 +10,15 @@ if (environment.production) {
   enableProdMode();
 }
 
+console.log("in main.js");
+
+Purchases.setup("api_key");
+Purchases.setDebugLogsEnabled(true);
+const userId = Purchases.getAppUserID();
+console.log("user ID is: " + userId);
+console.log("after setup");
+Purchases.setAttributes({"hey": "hi"});
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
